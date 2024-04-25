1c66f45f3ddb5b6457d91ae373d5c30aab6e1bd0
tammelin <tmmli@icloud.com>
2024-04-25 14:35:07 +0200

    Fixed worksamples archive

--- wp-content/themes/maria/archive-worksamples.php
+++ wp-content/themes/maria/archive-worksamples.php
@@ -6,34 +6,32 @@ $args = [
     'order' => 'ASC'
 ];
 $terms = get_terms($args);
-$worksamples_by_type = [];
-foreach($terms as $term) {
-    $args = [
-        'post_type' => 'worksamples',
-        'posts_per_page' => -1,
-        'tax_query' => [
-            [
-                'taxonomy' => 'worksampletype',
-                'field' => 'slug',
-                'terms' => $term->slug
-            ]
-        ]
-    ];
-    $worksamples_by_type[$term->name] = get_posts($args);
-}
 ?>
 
 <?php include('header.php'); ?>
 <main>
     <h1>Arbetsprover</h1>
     <div class="worksamples">
-        <?php foreach($worksamples_by_type as $type => $worksamples) : ?>
-            <h2><?php echo $type; ?></h2>
+        <?php foreach($terms as $term) : ?>
+            <h3 id="<?php echo $term->slug; ?>"><?php echo $term->name; ?></h3>
+            <?php
+            $args = [
+                'post_type' => 'worksamples',
+                'tax_query' => [
+                    [
+                        'taxonomy' => 'worksampletype',
+                        'field' => 'slug',
+                        'terms' => $term->slug
+                    ]
+                ]
+            ];
+            $posts = get_posts($args);
+            ?>
             <ul class="worksamples-list">
-                <?php foreach($worksamples as $worksample) : ?>
+                <?php foreach($posts as $post) : ?>
                     <li>
-                        <a href="<?php echo get_permalink($worksample->ID); ?>">
-                            <?php echo $worksample->post_title; ?>
+                        <a href="<?php echo get_permalink($post->ID); ?>">
+                            <?php echo $post->post_title; ?>
                         </a>
                     </li>
                 <?php endforeach; ?>
