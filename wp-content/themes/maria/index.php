3a8668390dba9fed3ded516a648101efcb855f51
tammelin <tmmli@icloud.com>
2024-05-07 12:30:27 +0200

    Fixed index.php and added simple pagination

--- wp-content/themes/maria/index.php
+++ wp-content/themes/maria/index.php
@@ -1,23 +1,26 @@
-<?php
-$args = [
-    'post_type' => 'blogg',
-    'numberposts' => -1,
-    'post_status' => 'publish',
-];
-$posts = get_post($args);
-?>
 <?php include('header.php'); ?>
-<div class="posts">
-    <div class="posts-grid">
+<div class="container">
+    <div class="posts">
         <h1>Kategorier</h1>
-       
-
-            <?php
-            wp_list_categories(array(
-                'title_li' => '',
-                'hide_empty' => 0,
-            ));
-            ?>
+        <?php
+        wp_list_categories(array(
+            'title_li' => '',
+            'hide_empty' => 0,
+        ));
+        ?>
+        <div class="posts-grid">
+            <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+                <article>
+                    <a href="<?php the_permalink(); ?>"><h1 class="post-title"><?php the_title(); ?></h1></a>
+                    <p><?php the_excerpt(); ?></p>
+                </article>
+            <?php endwhile; ?>
+            <div class="nav-previous alignleft"><?php previous_posts_link( 'Newer posts' ); ?></div>
+            <div class="nav-next alignright"><?php next_posts_link( 'Older posts' ); ?></div>
+            <?php else: ?>
+                <p>No posts found. 😢</p>
+            <?php endif; ?>
+        </div>
     </div>
 </div>
 <?php include('inc/logos.php'); ?>
