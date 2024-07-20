ed8af4c3689ea2edaf8398d71d4de59295276ccb
Pretrorian <jort2212@outlook.com>
2024-07-20 10:17:43 +0200

    Made changes in the code

--- wp-content/themes/maria/inc/post.php
+++ wp-content/themes/maria/inc/post.php
@@ -4,24 +4,33 @@ $featured_posts = get_field('featured_posts');
 <div class="posts container">
     <h1>Utvalda inlägg</h1>
     <div class="posts-grid">
-    <?php foreach($featured_posts as $post):
-        $post_image = get_the_post_thumbnail_url($post, 'featured_thumbnail');
+        <?php foreach ($featured_posts as $post) :
+            $post_image = get_the_post_thumbnail_url($post, 'featured_thumbnail');
         ?>
-        <div class="grid-item">
-            <div class="texter">
-            <?php if($post_image): ?>
-                <img src="<?php echo $post_image; ?>" alt="<?php echo $post->post_title; ?>">
-            <?php endif; ?>
-            <h3 class="text_tag">Aktuellt</h3>
-            <h2><?php echo $post->post_title; ?></h2>
-            <p><?php echo $post->post_content; ?></p>
-            </div> 
-        </div>
-    <?php endforeach; ?>
+            <div class="grid-item">
+                <div class="texter">
+                    <?php if ($post_image) : ?>
+                        <img src="<?php echo $post_image; ?>" alt="<?php echo $post->post_title; ?>">
+
+                    <?php endif; ?>
+                    <h3 class="text_tag">
+                        <?php
+                        $categories = get_the_category();
+                        if ( ! empty( $categories ) ) {
+                            foreach ( $categories as $category ) {
+                                echo '<a href="' . esc_url( get_category_link( $category->term_id ) ) 
+                                . '" class="mi-clase-css">' . esc_html( $category->name ) . '</a> ';
+                            }
+                        }
+                        ?>
+                    </h3>
+                    <h2><?php echo $post->post_title; ?></h2>
+                    <p><?php echo $post->post_content; ?></p>
+                </div>
+            </div>
+        <?php endforeach; ?>
     </div>
     <a href="http://localhost:3000/blogg/">
-    <button class="post_btn">Se alla inlägg</button>
-</a>
+        <button class="post_btn">Se alla inlägg</button>
+    </a>
 </div>
-
-
--- wp-content/themes/maria/single.php
+++ wp-content/themes/maria/single.php
@@ -7,12 +7,15 @@
         if ($categorias) {
             echo '<div class="post-categories">';
             foreach ($categorias as $categoria) {
-                echo '<a href="' . esc_url(get_category_link($categoria->term_id)) . '">' . esc_html($categoria->name) . '</a>';
+                echo '<a href="' . esc_url(get_category_link($categoria->term_id)) . '" class="category_title">' . esc_html($categoria->name) . '</a>';
             }
             echo '</div>';
         }
         ?>
         <h1 class="post-title"><?php the_title(); ?></h1>
+        <div>
+        <p><?php the_time('F j, Y'); ?> · <?php the_author_posts_link(); ?></p>
+        </div>
         <?php if (has_post_thumbnail()) : ?>
             <div class="post-thumbnail">
                 <?php the_post_thumbnail(); ?>
